2812da6b563a9de870922c6657b87b7335c7b374
Qingfeng Cai <francois_cai@hotmail.com>
2012-07-26 18:46:22 +0800

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roadclouding::Application.config.secret_token = 'f42bd0dd0b7b3e74dbfdb962beb423df6fd4a32722f9dc4711c429ba7c2e9146b655715947684e42ac67209274b99222dd7e410e82b72be06d493364cb6e78de'
