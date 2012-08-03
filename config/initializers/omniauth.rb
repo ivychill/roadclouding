4e4a28a1652b8b9adabbbc6ee7602794a3ac40d4
Qingfeng Cai <francois_cai@hotmail.com>
2012-08-03 11:42:19 +0800

    sina oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :weibo-oauth2, '4054595895', 'bc4d96b5235742487a559f43a8b585fd'  
+  provider :weibo, '4054595895', 'bc4d96b5235742487a559f43a8b585fd'  
 end
